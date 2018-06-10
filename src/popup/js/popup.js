5c6d65ad4993bd2736716d654e463b0a813cd8ca
Andrei Sabau <andrei@asabau.com>
2018-06-10 09:48:46 +0300

    admin area opens in new tab, closes #9

--- src/popup/js/popup.js
+++ src/popup/js/popup.js
@@ -1,1 +1,29 @@
-console.log("popup2.js")
+console.log('popup2.js');
+
+
+const port = chrome.runtime.connect({ name: 'gsf' });
+port.postMessage({ joke: 'Knock knock' });
+port.onMessage.addListener((msg) => {
+  console.log(`msg received at bkg: ${msg}`);
+  console.log(msg);
+  if (msg.question === "Who's there?") {
+    port.postMessage({ answer: 'Madame' });
+  }
+  else if (msg.question === 'Madame who?') {
+    port.postMessage({ answer: 'Madame... Bovary' });
+  }
+});
+
+function addAdminLink(container) {
+  const adminLink = document.createElement('a');
+  const linkText = document.createTextNode('Admin Area');
+  adminLink.appendChild(linkText);
+  adminLink.title = 'Admin Area';
+  adminLink.href = chrome.runtime.getURL('admin/admin.html');
+  adminLink.target = '_blank';
+  container.appendChild(adminLink);
+}
+
+
+addAdminLink(document.body);
+
