968aa40d64810a41052fa01d3c8b63a9ef34e9c6
Andrei Sabau <andrei@asabau.com>
2018-07-01 11:44:42 +0300

    re-organize tests based on main src structure

--- test/unit/gsf/storage/test-indexeddb-storage.js
+++ test/unit/gsf/storage/test-indexeddb-storage.js
@@ -1,9 +1,36 @@
+import NodeFetchPlugin from 'get-set-fetch/lib/plugins/fetch/NodeFetchPlugin';
+
+const sinon = require('sinon');
+
 const GetSetFetch = require('get-set-fetch');
+const JsDomPlugin = require('get-set-fetch/lib/plugins/process/JsDomPlugin');
+const ExtractUrlPlugin = require('get-set-fetch/lib/plugins/process/ExtractUrlPlugin');
 const ExternalStorageTests = require('get-set-fetch/test/external/external-storage-tests');
 
-const IdbStorage = gsfRequire('src/gsf/storage/IdbStorage');
+const IdbStorage = gsfRequire('./src/gsf/storage/IdbStorage');
+const ExtensionPluginManager = gsfRequire('./src/gsf/plugins/ExtensionPluginManager');
+const ExtensionFetchPlugin = gsfRequire('./src/gsf/plugins/fetch/ExtensionFetchPlugin');
+const ExtensionExtractUrlPlugin = gsfRequire('./src/gsf/plugins/process/ExtensionExtractUrlPlugin');
 
 const conn = { info: 'IndexedDB' };
+
+ExtensionPluginManager.registerDefaults();
+
+const nodeFetchPlugin = new NodeFetchPlugin();
+const extractUrlPlugin = new ExtractUrlPlugin();
+const jsdomPlugin = new JsDomPlugin();
+
+sinon.stub(ExtensionFetchPlugin.prototype, 'apply').callsFake((site, resource) => nodeFetchPlugin.apply(site, resource));
+sinon.stub(ExtensionExtractUrlPlugin.prototype, 'extractResourceUrls').callsFake((site, resource) => {
+  // init jsdom in order to parse html
+  const jsdomProps = jsdomPlugin.apply(site, resource);
+  const updatedResource = Object.assign(resource, jsdomProps);
+
+  // extract links
+  const { urlsToAdd } = extractUrlPlugin.apply(site, updatedResource);
+  return urlsToAdd;
+});
+
 
 /* functions needed by some tests, can't achieve the logic just using the Resource API */
 async function updateCrawledAt(IdbResource, resourceId, deltaHours) {
@@ -38,11 +65,9 @@ const ResourceFncs = {
   checkInitialCrawledAt,
 };
 
-console.log(IdbStorage.init);
-
 describe('Test Suite IndexedDB Storage', () => {
   Object.values(ExternalStorageTests).forEach((suite) => {
-    suite(GetSetFetch, IdbStorage, conn, ResourceFncs);
+    suite(GetSetFetch, ExtensionPluginManager, IdbStorage, conn, ResourceFncs);
   });
 });
 
