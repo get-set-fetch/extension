9e87c7069ae86ece32b70185be799ecdeef80dd4
Andrei Sabau <andrei@asabau.com>
2018-07-01 11:47:00 +0300

    ability to start crawling from project list, #12

--- src/admin/js/pages/projects/ProjectList.jsx
+++ src/admin/js/pages/projects/ProjectList.jsx
@@ -4,17 +4,31 @@ import Table from '../../components/Table';
 import GsfClient from '../../components/GsfClient';
 
 export default class ProjectList extends React.Component {
+  static async crawlSite(site) {
+    try {
+      await GsfClient.fetch('GET', `site/${site.id}/crawl`, { site, crawl: true });
+    }
+    catch (err) {
+      console.log('error crawling site');
+    }
+  }
+
   constructor(props) {
     super(props);
 
     this.state = {
       header: [
         {
-          label: 'name',
+          label: 'Name',
           prop: 'name',
           render: site => (<td><NavLink to={`/site/${site.id}`} className="nav-link">{site.name}</NavLink></td>),
         },
-        { label: 'url', prop: 'url' },
+        { label: 'URL', prop: 'url' },
+        { label: 'Status', prop: 'status' },
+        {
+          label: 'Actions',
+          render: site => (<td><input type="button" className="btn-secondary" value="Crawl" onClick={ProjectList.crawlSite.bind(this, site)}/></td>),
+        },
       ],
       data: [],
       currentSite: {},
@@ -22,17 +36,15 @@ export default class ProjectList extends React.Component {
   }
 
   async componentDidMount() {
-    const data = await GsfClient.fetch('GET', 'site');
+    const data = await GsfClient.fetch('GET', 'sites');
     this.setState({ data });
-    console.log(data);
   }
 
   // eslint-disable-next-line class-methods-use-this
   render() {
     return [
-      <NavLink to="/project" className="nav-link">Create new project</NavLink>,
-      <p>Project List</p>,
-      <Table header={this.state.header} data={this.state.data} />,
+      <p key="listHeader">Project List</p>,
+      <Table key="listTable" header={this.state.header} data={this.state.data} />,
     ];
   }
 }
