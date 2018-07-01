60d1af81e6dfdfbdd4e7fb22d821703d85a45889
Andrei Sabau <andrei@asabau.com>
2018-07-01 15:16:29 +0300

    basic Log List page, #23

---
+++ src/admin/js/pages/logs/LogList.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class LogList extends React.Component {
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    return (
+      <p>LogList</p>
+    );
+  }
+}
