a60ca587330e4d0f306c0cdb12f462ea6077137f
Andrei Sabau <andrei@asabau.com>
2018-07-22 17:07:53 +0300

    added row selection support, ability to define custom message when no data is present

--- src/admin/js/components/Table.js
+++ src/admin/js/components/Table.js
@@ -4,23 +4,69 @@ import PropTypes from 'prop-types';
 export default class Table extends React.Component {
   static get propTypes() {
     return {
+      onRowSelectionChange: PropTypes.func,
+      onHeaderSelectionChange: PropTypes.func,
+      selectedRows: PropTypes.arrayOf(PropTypes.number),
       header: PropTypes.arrayOf(PropTypes.object),
       data: PropTypes.arrayOf(PropTypes.object),
+      emptyTableMsg: PropTypes.string,
     };
   }
 
   static get defaultProps() {
     return {
+      onRowSelectionChange: null,
+      onHeaderSelectionChange: null,
+      selectedRows: [],
       header: [],
       data: [],
+      emptyTableMsg: 'No entries found',
     };
   }
 
+  constructor(props) {
+    super(props);
+    this.onCheckboxChange = this.onCheckboxChange.bind(this);
+  }
+
+  static toggleSelection(selectedRows, toggleRow) {
+    const finalSelection = selectedRows.slice();
+
+    const idx = finalSelection.indexOf(toggleRow);
+    if (idx !== -1) {
+      finalSelection.splice(idx, 1);
+    }
+    else {
+      finalSelection.push(toggleRow);
+    }
+
+    return finalSelection;
+  }
+
+  onCheckboxChange(evt) {
+    const selectedIdx = parseInt(evt.target.value, 10);
+    this.props.onRowSelectionChange(selectedIdx);
+  }
+
   render() {
+    return this.props.data && this.props.data.length > 0 ? this.renderTable() : this.renderEmptyMsg();
+  }
+
+  renderTable() {
     return (
       <table className="table table-hover table-fixed">
         <thead className="blue-grey lighten-4">
           <tr>
+            {
+              this.props.onHeaderSelectionChange && <th key="checkbox">
+              <input
+                className="form-check-input"
+                type="checkbox"
+                onChange={this.props.onHeaderSelectionChange}
+                checked={this.props.selectedRows.length === this.props.data.length}
+              />
+            </th>
+            }
             {
               this.props.header.map(hr => <th key={hr.label}>{hr.label}</th>)
             }
@@ -28,12 +74,22 @@ export default class Table extends React.Component {
         </thead>
         <tbody>
           {
-            this.props.data.map(row => (
+            this.props.data.map((row, rowIdx) => (
               <tr key={row.toString()}>
                 {
-                  this.props.header.map(hr => (
-                    hr.render ? hr.render(row) : <td>{row[hr.prop]}</td>
-                  ))
+                  this.props.onRowSelectionChange &&
+                  <td>
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value={rowIdx}
+                      onChange={this.onCheckboxChange}
+                      checked={this.props.selectedRows.indexOf(rowIdx) !== -1}
+                    />
+                </td>
+                }
+                {
+                  this.props.header.map(hr => (hr.render ? hr.render(row) : <td>{row[hr.prop]}</td>))
                 }
               </tr>
             ))
@@ -41,5 +97,9 @@ export default class Table extends React.Component {
         </tbody>
       </table>
     );
+  }
+
+  renderEmptyMsg() {
+    return <p><i>{this.props.emptyTableMsg}</i></p>;
   }
 }
