d41f661234afd37efd003f5d69fd249203985b13
Andrei Sabau <andrei@asabau.com>
2019-02-24 18:14:05 +0200

    added plugin for extracting image info, #31

---
+++ packages/background/src/ts/plugins/builtin/ImageFilterPlugin.ts
@@ -0,0 +1,48 @@
+import SchemaHelper from '../../schema/SchemaHelper';
+import { IPlugin, ISite, IResource } from 'get-set-fetch';
+
+export default class ImageFilterPlugin implements IPlugin {
+  opts: {
+    runInTab: boolean
+  };
+
+  constructor(opts) {
+    this.opts = SchemaHelper.instantiate(ImageFilterPlugin.OPTS_SCHEMA, opts);
+  }
+
+  static get OPTS_SCHEMA() {
+    return {
+      $id: 'https://getsetfetch.org/extract-url-plugin.schema.json',
+      $schema: 'http://json-schema.org/draft-07/schema#',
+      title: 'ImageFilterPlugin',
+      type: 'object',
+      properties: {
+        runInTab: {
+          type: 'boolean',
+          default: false
+        }
+      }
+    };
+  }
+
+  test(resource: IResource) {
+    return resource.mediaType.indexOf('image') !== -1;
+  }
+
+  apply(site: ISite, resource: IResource) {
+    return new Promise((resolve, reject) => {
+      const img = document.createElement('img');
+      img.onload = (evt: Event) => {
+        window.URL.revokeObjectURL(img.src);
+        resolve({
+          info: {
+            width: img.naturalWidth,
+            height: img.naturalHeight
+          }
+        });
+      };
+      img.onerror = () => reject('could not load image: ' + img.src);
+      img.src = window.URL.createObjectURL(resource.blob);
+    });
+  }
+}
