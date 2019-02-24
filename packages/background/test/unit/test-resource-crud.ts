40b13483a7b51ca0514b5e75dc0aabaa24d0e8c8
Andrei Sabau <andrei@asabau.com>
2019-02-24 18:10:47 +0200

    test updates reflecting resource new blob and mediaType properties, #31

--- packages/background/test/unit/test-resource-crud.ts
+++ packages/background/test/unit/test-resource-crud.ts
@@ -1,18 +1,19 @@
 import { assert } from 'chai';
 import IdbStorage from '../../src/ts/storage/IdbStorage';
+import IdbResource from '../../src/ts/storage/IdbResource';
 
 const conn = { info: 'IndexedDB' };
 
 describe(`Test Storage Resource - CRUD, using connection ${conn.info}`, () => {
   let Site = null;
-  let Resource = null;
+  let Resource: typeof IdbResource;
   const expectedResource = {
       id: null,
       siteId: null,
       url: 'http://siteA/resourceA',
       info: { propA: 'valA' },
-      content: Buffer.from('contentA'),
-      contentType: 'text/plain'
+      blob: Buffer.from('contentA'),
+      mediaType: 'text/plain'
     };
 
   before(async () => {
@@ -31,8 +32,8 @@ describe(`Test Storage Resource - CRUD, using connection ${conn.info}`, () => {
       // save resource
       const resource = new Resource({ siteId: expectedResource.siteId, url: expectedResource.url });
       resource.info = expectedResource.info;
-      resource.content = expectedResource.content;
-      resource.contentType = expectedResource.contentType;
+      resource.blob = expectedResource.blob;
+      resource.mediaType = expectedResource.mediaType;
       await resource.save();
       assert.isNotNull(resource.id);
       expectedResource.id = resource.id;
@@ -65,8 +66,8 @@ describe(`Test Storage Resource - CRUD, using connection ${conn.info}`, () => {
       assert.strictEqual(String(expectedResource.siteId), String(resourceById.siteId));
       assert.strictEqual(expectedResource.url, resourceById.url);
       assert.strictEqual(expectedResource.info.propA, resourceById.info.propA);
-      assert.strictEqual(true, expectedResource.content.equals(resourceById.content));
-      assert.strictEqual(expectedResource.contentType, resourceById.contentType);
+      assert.strictEqual(true, expectedResource.blob.equals(resourceById.blob));
+      assert.strictEqual(expectedResource.mediaType, resourceById.mediaType);
 
       // get resource by url
       const resourceByUrl = await Resource.get(expectedResource.url);
@@ -74,8 +75,8 @@ describe(`Test Storage Resource - CRUD, using connection ${conn.info}`, () => {
       assert.strictEqual(String(expectedResource.id), String(resourceByUrl.id));
       assert.strictEqual(String(expectedResource.siteId), String(resourceByUrl.siteId));
       assert.strictEqual(expectedResource.info.propA, resourceByUrl.info.propA);
-      assert.strictEqual(true, expectedResource.content.equals(resourceById.content));
-      assert.strictEqual(expectedResource.contentType, resourceById.contentType);
+      assert.strictEqual(true, expectedResource.blob.equals(resourceById.blob));
+      assert.strictEqual(expectedResource.mediaType, resourceById.mediaType);
     });
 
   it('update', async () => {
@@ -83,38 +84,38 @@ describe(`Test Storage Resource - CRUD, using connection ${conn.info}`, () => {
       const updateResource = await Resource.get(expectedResource.id);
       updateResource.url = 'http://siteA/resourceA_updated';
       updateResource.info = { propA: 'valA_changed' };
-      updateResource.content = 'contentA_changed';
+      updateResource.blob = 'contentA_changed';
       await updateResource.update();
-      updateResource.content = 'contentA_changed1';
+      updateResource.blob = 'contentA_changed1';
 
       // get and compare
       const getResource = await Resource.get(expectedResource.id);
       assert.strictEqual(String(expectedResource.siteId), String(getResource.siteId));
       assert.strictEqual(updateResource.url, getResource.url);
       assert.strictEqual(updateResource.info.propA, getResource.info.propA);
-      assert.strictEqual(updateResource.contentType, getResource.contentType);
+      assert.strictEqual(updateResource.mediaType, getResource.mediaType);
     });
 
   it('update binary content', async () => {
       // update resource
-      const updateResource = await Resource.get(expectedResource.id);
-      updateResource.content = Buffer.alloc(2, 1);
-      updateResource.contentType = 'utf8';
+      const updateResource: IdbResource = await Resource.get(expectedResource.id);
+      updateResource.blob = Buffer.alloc(2, 1);
+      updateResource.mediaType = 'utf8';
       await updateResource.update();
 
       // get and compare
-      const getResource = await Resource.get(expectedResource.id);
-      assert.strictEqual(true, updateResource.content.equals(getResource.content));
-      assert.strictEqual(updateResource.contentType, getResource.contentType);
+      const getResource: IdbResource  = await Resource.get(expectedResource.id);
+      assert.strictEqual(true, updateResource.blob.equals(getResource.blob));
+      assert.strictEqual(updateResource.mediaType, getResource.mediaType);
     });
 
   it('delete', async () => {
       // delete site
-      const delResource = await Resource.get(expectedResource.id);
+      const delResource: IdbResource  = await Resource.get(expectedResource.id);
       await delResource.del();
 
       // get and compare
-      const getResource = await Resource.get(expectedResource.id);
+      const getResource: IdbResource  = await Resource.get(expectedResource.id);
       assert.isNull(getResource);
     });
 });
