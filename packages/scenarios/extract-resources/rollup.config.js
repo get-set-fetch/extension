ce1823ec0579a67afcf9bcbaf1a00c4a4588bca7
Andrei Sabau <andrei@asabau.com>
2019-03-10 19:54:42 +0200

    dependencies update, #31

--- packages/scenarios/extract-resources/rollup.config.js
+++ packages/scenarios/extract-resources/rollup.config.js
@@ -1,5 +1,4 @@
 import commonjs from 'rollup-plugin-commonjs';
-import json from 'rollup-plugin-json';
 import typescript from 'rollup-plugin-typescript';
 import resolve from 'rollup-plugin-node-resolve';
 import tslint from 'rollup-plugin-tslint';
