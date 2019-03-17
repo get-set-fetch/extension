e3066585023ff795a6d41317965b524e162cec7c
Andrei Sabau <andrei@asabau.com>
2019-03-17 19:47:45 +0200

    removed typings now present in commons

--- packages/background/typings/get-set-fetch/index.d.ts
+++ packages/background/typings/get-set-fetch/index.d.ts
@@ -29,49 +29,5 @@ declare module 'get-set-fetch' {
     static create(maxEntries:number, probability:number, bitset:Buffer)
     constructor(m:number, k:number, bitset:Buffer)
   }
-
-  export interface IResource {
-    crawledAt: any;
-    id: number;
-    url: string;
-    crawlInProgress: boolean;
-    depth: number;
-    siteId: number;
-    mediaType: string;
-    blob: any;
-    info: any;
-  }
-
-  export interface IPluginDefinition {
-    name: string;
-    opts: object;
-  }
-
-  export interface ISite {
-    id: number;
-    projectId: number;
-    name: string;
-    url: string;
-    tabId: any;
-
-    pluginDefinitions: IPluginDefinition[];
-    plugins: any;
-
-    opts: {
-      crawl: {
-        maxConnections: number,
-        maxResources: number,
-        delay: number
-      },
-      resourceFilter: {
-        maxEntries: number,
-        probability: number
-      }
-    };
-  }
-
-  export interface IPlugin {
-    test(resource:IResource):boolean;
-    apply(site:ISite, resource: IResource):any;
-  }
+ 
  }
