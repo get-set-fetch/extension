94cf82c16dcde38ec57a1f4dea65ba39e1662ec2
Andrei Sabau <andrei@asabau.com>
2019-03-24 20:50:18 +0200

    mocked sites are now available both from http and https, #32

--- packages/extension/test/utils/init-proxy.ts
+++ packages/extension/test/utils/init-proxy.ts
@@ -1,14 +1,22 @@
 import connect from 'connect';
 import serveStatic from 'serve-static';
 import vhost from 'vhost';
+import http from 'http';
+import https from 'https';
+import CertGenerator from './CertGenerator';
 
 const app = connect();
 
-// serve siteA
+// serve siteA used for crawling
 const siteA = connect();
-siteA.use(serveStatic('./test/resources/siteA'));
+siteA.use(serveStatic('./test/resources/sites/sitea.com'));
 app.use(vhost('sitea.com', siteA));
 app.use(vhost('www.sitea.com', siteA));
 
-app.listen(3000);
-console.log('proxy started on 3000');
+
+// serve the content over http
+http.createServer(app).listen(80);
+
+// server the content over https
+const tlsOpts = CertGenerator.generate();
+https.createServer(tlsOpts, app).listen(443);
