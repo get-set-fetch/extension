cb52de948246b5fecd6b5c836adbcdc5d5caead2
Andrei Sabau <andrei@asabau.com>
2019-04-14 16:24:18 +0300

    handle install / uninstall events

--- packages/background/src/ts/background-main.ts
+++ packages/background/src/ts/background-main.ts
@@ -5,18 +5,24 @@ import ScenarioManager from './scenarios/ScenarioManager';
 const Log = Logger.getLogger('background-main');
 
 (async () => {
-  await GsfProvider.init();
+  try {
+    // 0. handle install / uninstall events
+    chrome.runtime.onInstalled.addListener(details => {
+      chrome.tabs.create({ url: 'https://getsetfetch.org/extension/thank-you-install.html' });
+    });
+    chrome.runtime.setUninstallURL('https://getsetfetch.org/extension/thank-you-uninstall.html');
+
+    await GsfProvider.init();
 
   // 1. populate settings - logLevel - if not present
-  const storedSettings = await GsfProvider.Setting.getAll();
-  let logLevel = storedSettings.find(setting => setting.key === 'logLevel');
-  if (!logLevel) {
+    const storedSettings = await GsfProvider.Setting.getAll();
+    let logLevel = storedSettings.find(setting => setting.key === 'logLevel');
+    if (!logLevel) {
     logLevel = new GsfProvider.Setting({ key: 'logLevel', val: LogLevel.WARN.toString() });
     await logLevel.save();
   }
-  Logger.setLogLevel(parseInt(logLevel.val, 10));
+    Logger.setLogLevel(parseInt(logLevel.val, 10));
 
-  try {
     // 2. read all builtin plugins, persist them as Plugin
     await PluginManager.discoverLocalPlugins();
 
