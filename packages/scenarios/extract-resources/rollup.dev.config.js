d98ee9f583b73604462e9f2114ffa655be943764
Andrei Sabau <andrei@asabau.com>
2019-04-21 17:30:49 +0300

    npm audit fixes

--- packages/scenarios/extract-resources/rollup.dev.config.js
+++ packages/scenarios/extract-resources/rollup.dev.config.js
@@ -4,7 +4,6 @@ import typescript from 'rollup-plugin-typescript';
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
 import globals from 'rollup-plugin-node-globals';
-import builtins from 'rollup-plugin-node-builtins';
 import serve from 'rollup-plugin-serve';
 import tslint from 'rollup-plugin-tslint';
 import copy from 'rollup-plugin-copy';
@@ -36,7 +35,6 @@ export default {
       jsnext: true,
     }),
     globals(),
-    builtins(),
     tslint(),
     copy({
       "dev/index.html": "dev/serve/index.html",
