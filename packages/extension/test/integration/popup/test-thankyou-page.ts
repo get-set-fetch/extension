0a10bb4d6b14fdcb97696d148f2c48b657acf4b4
Andrei Sabau <andrei@asabau.com>
2019-05-05 11:42:55 +0300

    new test(s)

---
+++ packages/extension/test/integration/popup/test-thankyou-page.ts
@@ -0,0 +1,20 @@
+import { assert } from 'chai';
+import BrowserHelper from '../../helpers/BrowserHelper';
+
+describe('Test ThankYou Pages', () => {
+  let browserHelper: BrowserHelper;
+
+  before(async () => {
+    browserHelper = await BrowserHelper.launch(false);
+  });
+
+  after(async () => {
+    // await browserHelper.close();
+  });
+
+  it('install page', async () => {
+    const tyPage = (await browserHelper.browser.pages()).find(page => /thank/.test(page.url()));
+    assert.isDefined(tyPage);
+    assert.equal(tyPage.url(), 'https://getsetfetch.org/extension/thank-you-install.html');
+  });
+});
