1089c56be5be5138d51033be7e8a197e70a14611
Andrei Sabau <andrei@asabau.com>
2019-06-23 09:36:35 +0300

    code cleanup, additional unit tests

--- packages/background/test/unit/plugins/test-extract-urls-plugin.ts
+++ packages/background/test/unit/plugins/test-extract-urls-plugin.ts
@@ -5,7 +5,7 @@ import ExtractUrlsPlugin from '../../../src/ts/plugins/builtin/ExtractUrlsPlugin
 describe('Test Extract Urls Plugin', () => {
   let sandbox;
   let stubDocument;
-  let extractUrlsPlugin;
+  let extractUrlsPlugin: ExtractUrlsPlugin;
 
   beforeEach(() => {
     sandbox = createSandbox();
@@ -18,8 +18,8 @@ describe('Test Extract Urls Plugin', () => {
 
   it('test media type - default options', () => {
     extractUrlsPlugin = new ExtractUrlsPlugin({});
-    assert.isTrue(extractUrlsPlugin.test({ mediaType: 'text/html' }));
-    assert.isFalse(extractUrlsPlugin.test({ mediaType: 'text/plain' }));
+    assert.isTrue(extractUrlsPlugin.test({ mediaType: 'text/html' } as any));
+    assert.isFalse(extractUrlsPlugin.test({ mediaType: 'text/plain' } as any));
   });
 
   it('extract unique urls - default options', () => {
@@ -29,18 +29,19 @@ describe('Test Extract Urls Plugin', () => {
       { href: 'http://sitea.com/page1.html' },
       { href: 'http://sitea.com/page1.html#fragment1' },
       { href: 'http://sitea.com/page1.html#fragment2' },
-      { href: 'http://sitea.com/page1.html' }
+      { href: 'http://sitea.com/page1.html' },
     ]);
 
     stubDocument.withArgs('img').returns([
       { src: 'http://sitea.com/img1.png' },
-      { src: 'http://sitea.com/img1.png' }
+      { src: 'http://sitea.com/img1.png' },
     ]);
 
     const expectedValidUrls = [
-      'http://sitea.com/page1.html'
+      'http://sitea.com/page1.html',
     ];
-    const { urlsToAdd } = extractUrlsPlugin.apply(null, { url: 'http://sitea.com/index.html', depth: 1 });
+    // eslint-disable-next-line prefer-spread
+    const { urlsToAdd } = extractUrlsPlugin.apply(null, { url: 'http://sitea.com/index.html', depth: 1 } as any);
     assert.sameMembers(urlsToAdd, expectedValidUrls);
   });
 
@@ -49,20 +50,43 @@ describe('Test Extract Urls Plugin', () => {
 
     stubDocument.withArgs('a').returns([
       { href: 'http://sitea.com/page1.html' },
-      { href: 'http://sitea.com/page1.html' }
+      { href: 'http://sitea.com/page1.html' },
     ]);
 
     stubDocument.withArgs('img').returns([
       { src: 'http://sitea.com/img1.png' },
-      { src: 'http://sitea.com/img1.png' }
+      { src: 'http://sitea.com/img1.png' },
     ]);
 
     const expectedValidUrls = [
       'http://sitea.com/page1.html',
-       'http://sitea.com/img1.png'
+      'http://sitea.com/img1.png',
     ];
-    const { urlsToAdd } = extractUrlsPlugin.apply(null, { url: 'http://sitea.com/index.html', depth: 1 });
+    // eslint-disable-next-line prefer-spread
+    const { urlsToAdd } = extractUrlsPlugin.apply(null, { url: 'http://sitea.com/index.html', depth: 1 } as any);
     assert.sameMembers(urlsToAdd, expectedValidUrls);
+  });
+
+  it('compare hostnames', () => {
+    extractUrlsPlugin = new ExtractUrlsPlugin({});
+
+    // same hostnames, some domain
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('domainA.com', 'domainA.com'));
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('domA.com', 'domA.com'));
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('sub1.domainA.com', 'sub1.domainA.com'));
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('sub1.sub2.domainA.com', 'sub1.sub2.domainA.com'));
+
+    // different hostnames, same domain
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('subA.domainA.com', 'subB.domainA.com'));
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('subA1.subA2.domainA.com', 'subB1.subB2.domainA.com'));
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('subA.domainA.com', 'domainA.com'));
+    assert.isTrue(extractUrlsPlugin.sameDomainHostnames('subA1.subA2.domainA.com', 'domainA.com'));
+
+    // different hostnames, different domains
+    assert.isFalse(extractUrlsPlugin.sameDomainHostnames('domainA.com', 'domainB.com'));
+    assert.isFalse(extractUrlsPlugin.sameDomainHostnames('domA.com', 'domB.com'));
+    assert.isFalse(extractUrlsPlugin.sameDomainHostnames('sub1.domainA.com', 'sub1.domainB.com'));
+    assert.isFalse(extractUrlsPlugin.sameDomainHostnames('sub1.sub2.domainA.com', 'sub1.sub2.domainB.com'));
   });
 
   // it('extract unique urls - restricted by maxDepth', () => {
