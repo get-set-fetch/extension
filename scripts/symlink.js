35f5f97a9c3763d2c735619ef6177e230dfbff34
Andrei Sabau <andrei@asabau.com>
2019-09-01 16:19:32 +0300

    differentiate between creating and deleting symlinks

--- scripts/symlink.js
+++ scripts/symlink.js
@@ -1,15 +1,25 @@
 const path = require('path');
 const shell = require('shelljs');
 
-const pkgFiles = shell.find('./packages').filter(function(file) { return file.match(/package.json$/); });
-const pkgDirs = pkgFiles.map(pkgFile => path.dirname(pkgFile)).filter(pkgDir => !/(node_modules|dist|test)/.test(pkgDir));
+const action = process.argv[2];
 
+const pkgFiles = shell.find('./packages').filter(function(file) { return file.match(/package.json$/); });
+const pkgDirs = pkgFiles.map(pkgFile => path.dirname(pkgFile)).filter(pkgDir => !/(node_modules|dist\/|test\/)/.test(pkgDir));
 
 pkgDirs.forEach(pkgDir => {
   const rootNodeModulesPath = path.join(process.cwd(), 'node_modules');
   const pkgNodeModulesPath = path.join(process.cwd(), pkgDir, 'node_modules');
 
-  console.log(`re-linking ${pkgNodeModulesPath}`)
-  shell.rm('-rf', pkgNodeModulesPath);
-  shell.ln('-sf', rootNodeModulesPath, pkgNodeModulesPath);
+  switch (action) {
+    case "create":
+      console.log(`linking ${pkgNodeModulesPath}`)
+      shell.ln('-sf', rootNodeModulesPath, pkgNodeModulesPath);
+      break;
+    case "delete":
+      console.log(`removing ${pkgNodeModulesPath}`)
+      shell.rm('-rf', pkgNodeModulesPath);
+      break;
+    default:
+      console.log(`action not recognized: ${action}`)
+  }
 })
