fcd925d976071d511b3f478e6f202bf25ba7f457
Andrei Sabau <andrei@asabau.com>
2019-09-08 20:10:41 +0300

    npm package release candidate version is now compatible with browser manifest version

--- packages/extension/scripts/manifest-update.js
+++ packages/extension/scripts/manifest-update.js
@@ -1,4 +1,4 @@
 const shell = require('shelljs');
 
-const pkgVersion = process.env.npm_package_version;
+const pkgVersion = process.env.npm_package_version.replace("-rc", "");
 shell.exec(`shx sed -i s/{GSF_VERSION}/${pkgVersion}/ dist/manifest.json`);
