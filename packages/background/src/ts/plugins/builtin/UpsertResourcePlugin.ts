b29302f09669e5317b1aa7befb1dbd877238e77a
Andrei Sabau <andrei@asabau.com>
2020-03-28 19:23:53 +0200

    UpdateResourcePlugin becomes UpsertResourcePlugin and can both insert or update scrapped resources

---
+++ packages/background/src/ts/plugins/builtin/UpsertResourcePlugin.ts
@@ -0,0 +1,38 @@
+/* eslint-disable no-param-reassign */
+import { BasePlugin, IEnhancedJSONSchema, ISite, IResource } from 'get-set-fetch-extension-commons';
+import IdbResource from '../../storage/IdbResource';
+
+/**
+ * Plugin responsible for updating a resource after crawling it.
+ */
+export default class UpsertResourcePlugin extends BasePlugin {
+  getOptsSchema(): IEnhancedJSONSchema {
+    return {
+      type: 'object',
+      title: 'Upsert Resource Plugin',
+      description: 'responsible for updating a resource at storage level after scraping it.',
+    };
+  }
+
+  test(site: ISite, resource: IResource) {
+    // only update a currently crawled resource
+    return resource && resource.crawlInProgress;
+  }
+
+  apply(site: ISite, resource: IdbResource) {
+    // static resources have already been inserted in db via plugins like InsertResourcesPlugin in a previous crawl step, just do update
+    if (resource.id) {
+      return resource.update();
+    }
+
+    /*
+    dynamic resources are found and scrapped on the fly starting from an already scrapped static resource,
+    disable crawlInProgressFlag, set crawledAt date
+    do save
+    the site bloom filter is not updated, only static resources are filtered against it
+    */
+    resource.crawlInProgress = false;
+    resource.crawledAt = new Date();
+    return resource.save();
+  }
+}
