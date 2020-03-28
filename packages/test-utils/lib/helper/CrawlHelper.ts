051a857ec5db9d8299363611693c745d374177e4
Andrei Sabau <andrei@asabau.com>
2020-03-28 19:39:44 +0200

    crawling is considered complete when both static and dynamic resources are succesfully scraped

--- packages/test-utils/lib/helper/CrawlHelper.ts
+++ packages/test-utils/lib/helper/CrawlHelper.ts
@@ -1,24 +1,31 @@
 declare const GsfClient;
 export default class CrawlHelper {
-  static async waitForCrawlComplete(page, siteId, resolve = null) {
+  static async waitForCrawlComplete(page, siteId, prevResourceNo = null, resolve = null) {
     // if no promise defined return one
     if (!resolve) {
       return new Promise(resolve => {
-        setTimeout(CrawlHelper.waitForCrawlComplete, 5000, page, siteId, resolve);
+        setTimeout(CrawlHelper.waitForCrawlComplete, 5000, page, siteId, null, resolve);
       });
     }
 
+    /*
+    check if crawl complete
+    for static resources (2 db steps: insert not crawled resources, update resource to crawled status)
+      - total resources equals crawled resources -> crawl complete
+    for dynamic resources (1 db step:  resource is directly saved with crawled status and scrapped content)
+      - no new resources have been added -> crawl complete
+    */
     const allResources = await page.evaluate(siteId => GsfClient.fetch('GET', `resources/${siteId}`), siteId);
     const crawledResources = await page.evaluate(siteId => GsfClient.fetch('GET', `resources/${siteId}/crawled`), siteId);
 
-    const notCrawledNo = allResources.length - crawledResources.length;
+    const staticCrawlComplete = allResources.length === crawledResources.length;
+    const dynamicCrawlComplete = prevResourceNo === allResources.length;
 
-    // crawl complete, there are no more resources to be crawled
-    if (notCrawledNo === 0) {
+    if (staticCrawlComplete && dynamicCrawlComplete) {
       resolve();
     }
     else {
-      setTimeout(CrawlHelper.waitForCrawlComplete, 5000, page, siteId, resolve);
+      setTimeout(CrawlHelper.waitForCrawlComplete, 5000, page, siteId, allResources.length, resolve);
     }
 
     return null;
@@ -29,7 +36,7 @@ export default class CrawlHelper {
     let actualResources = await page.evaluate(siteId => GsfClient.fetch('GET', `resources/${siteId}/crawled`), siteId);
 
     // only keep the properties we're interested in
-    actualResources = actualResources.map(({ url, mediaType, info }) => ({ url, mediaType, info }));
+    actualResources = actualResources.map(({ url, actions, mediaType, meta, content }) => ({ url, mediaType, meta, content, actions }));
 
     return actualResources;
   }
