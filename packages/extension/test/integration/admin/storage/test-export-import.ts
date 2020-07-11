2385cf78a358aadb19a92daf73c7ebb955f0b348
Andrei Sabau <andrei@asabau.com>
2020-07-11 22:39:15 +0300

    integration tests #42

--- packages/extension/test/integration/admin/storage/test-export-import.ts
+++ packages/extension/test/integration/admin/storage/test-export-import.ts
@@ -1,30 +1,14 @@
 /* eslint-disable no-param-reassign */
-import { readFileSync } from 'fs';
 import { assert } from 'chai';
 import { Page } from 'puppeteer';
 import { resolve } from 'path';
-import { LogLevel } from 'get-set-fetch-extension-commons';
+import { LogLevel, ILog } from 'get-set-fetch-extension-commons';
 import { BrowserHelper } from 'get-set-fetch-extension-test-utils';
-
 
 describe('Storage', () => {
   let browserHelper: BrowserHelper;
   let page: Page;
   const targetDir = resolve(process.cwd(), 'test', 'tmp');
-
-  const expectedJsonContent = {
-    Logs: [ {
-      level: 3,
-      cls: 'cls3',
-      msg: [ 'WARN message' ],
-    },
-    {
-      level: 4,
-      cls: 'cls4',
-      msg: [ 'ERROR message' ],
-    },
-    ],
-  };
 
   before(async () => {
     const extensionPath = resolve(process.cwd(), 'node_modules', 'get-set-fetch-extension', 'dist');
@@ -44,10 +28,13 @@ describe('Storage', () => {
   });
 
   after(async () => {
-    await browserHelper.close();
+    // await browserHelper.close();
   });
 
-  it('Populate > Export > Clear > Import Logs', async () => {
+  it('Clear > Populate > Export > Clear > Import Logs', async () => {
+    // 0. Clear
+    await page.evaluate(() => GsfClient.fetch('DELETE', 'logs'));
+
     // 1. Populate
     // do logging on WARN and higher, levels recorded by default settings
     const logLevels = [ 3, 4 ];
@@ -69,19 +56,10 @@ describe('Storage', () => {
     await page.click(exportLink);
 
     // wait a bit for file to be generated and saved
-    await new Promise(res => setTimeout(res, 1000));
+    await new Promise(res => setTimeout(res, 5000));
 
-    // read content
-    const rawContent = readFileSync(resolve(targetDir, 'gsf-export.json'), 'utf8');
-    const jsonContent = JSON.parse(rawContent);
-
-    jsonContent.Logs.forEach(logEntry => {
-      delete logEntry.id;
-      delete logEntry.date;
-    });
-    assert.deepEqual(jsonContent, expectedJsonContent);
-
-    // 3. Clear
+    // 3. Get expected values and clear
+    const expectedLogs: ILog[] = await page.evaluate(() => GsfClient.fetch('GET', 'logs'));
     await page.evaluate(() => GsfClient.fetch('DELETE', 'logs'));
 
     // check entries have been deleted, p.no-entries only appears when store is empty
@@ -92,7 +70,6 @@ describe('Storage', () => {
     // open settings page
     await browserHelper.goto('/settings');
 
-    /*
     // upload data to be imported
     const [ fileChooser ] = await Promise.all([
       page.waitForFileChooser(),
@@ -100,13 +77,11 @@ describe('Storage', () => {
     ]);
     await fileChooser.accept([ resolve(targetDir, 'gsf-export.json') ]);
 
+    // wait for file to be imported
+    await browserHelper.waitFor('p#success', 5000);
+
     // compare logs
-    const importedLogs = await page.evaluate(() => GsfClient.fetch('GET', 'logs'));
-    importedLogs.Logs.forEach(logEntry => {
-      delete logEntry.id;
-      delete logEntry.date;
-    });
-    assert.deepEqual(importedLogs, expectedJsonContent.Logs);
-    */
+    const importedLogs: ILog[] = await page.evaluate(() => GsfClient.fetch('GET', 'logs'));
+    assert.sameDeepMembers(importedLogs, expectedLogs);
   });
 });
