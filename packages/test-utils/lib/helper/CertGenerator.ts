1a8ffe21939ff7da7fb7f04367756895e3b77d20
Andrei Sabau <andrei@asabau.com>
2020-07-18 13:06:56 +0300

    generate ca and web srv certs #40

--- packages/test-utils/lib/helper/CertGenerator.ts
+++ packages/test-utils/lib/helper/CertGenerator.ts
@@ -1,39 +1,44 @@
-import forge from 'node-forge';
+import { pki, pkcs12, asn1, md } from 'node-forge';
+
+export interface ITlsOptions {
+  privateKey: string;
+  publicKeyCertificate: string;
+  privateKeyCertificate: string;
+}
 
 export default class CertGenerator {
-  static generate() {
-    const { pki } = forge;
+  static generateCertificate(
+    subjectAttrs: pki.CertificateField[],
+    issuerAttrs: pki.CertificateField[],
+    extensions: any[],
+    pemSignKey: string = null,
+    caCertPem: string = null,
+  ): ITlsOptions {
     const keys = pki.rsa.generateKeyPair(1024);
     const cert = pki.createCertificate();
 
     cert.publicKey = keys.publicKey;
-    cert.serialNumber = '01';
+    cert.serialNumber = pemSignKey ? '01' : '02';
     cert.validity.notBefore = new Date();
     cert.validity.notAfter = new Date();
     cert.validity.notAfter.setFullYear(cert.validity.notBefore.getFullYear() + 1);
 
-    const attrs = [
-      { name: 'commonName', value: 'get-set-fetch.org' },
-      { name: 'countryName', value: 'RO' },
-      { shortName: 'ST', value: 'Bucharest' },
-      { name: 'localityName', value: 'Bucharest' },
-      { name: 'organizationName', value: 'gsf' },
-      { shortName: 'OU', value: 'gsf' },
-    ];
-    cert.setSubject(attrs);
-    cert.setIssuer(attrs);
-    cert.sign(keys.privateKey);
+    cert.setSubject(subjectAttrs);
+    cert.setIssuer(issuerAttrs);
+    cert.setExtensions(extensions);
+    cert.sign(pemSignKey ? pki.privateKeyFromPem(pemSignKey) : keys.privateKey, md.sha256.create());
 
-    const tlsOtps = {
-      key: pki.privateKeyToPem(keys.privateKey),
-      cert: pki.certificateToPem(cert),
-      ca: pki.certificateToPem(cert),
-    };
+    const certChain = caCertPem ? [ pki.certificateFromPem(caCertPem), cert ] : [ cert ];
+    const p12Asn1 = pkcs12.toPkcs12Asn1(
+      keys.privateKey, certChain, 'password',
+      { algorithm: '3des' },
+    );
+    const p12Der = asn1.toDer(p12Asn1).getBytes();
 
     return {
-      key: tlsOtps.key,
-      cert: tlsOtps.cert,
-      ca: tlsOtps.cert,
+      privateKey: pki.privateKeyToPem(keys.privateKey),
+      publicKeyCertificate: pki.certificateToPem(cert),
+      privateKeyCertificate: p12Der,
     };
   }
 }
