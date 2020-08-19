c24004ae95899ad57c76c92e577895c43a997d86
Andrei Sabau <andrei@asabau.com>
2020-08-19 21:37:45 +0300

    single lint config file for the entire monorepo

--- scripts/symlink.js
+++ scripts/symlink.js
@@ -10,14 +10,19 @@ pkgDirs.forEach(pkgDir => {
   const rootNodeModulesPath = path.join(process.cwd(), 'node_modules');
   const pkgNodeModulesPath = path.join(process.cwd(), pkgDir, 'node_modules');
 
+  const rootLintPath = path.join(process.cwd(), '.eslintrc.json');
+  const pkgLintPath =  path.join(process.cwd(), pkgDir, '.eslintrc.json');
+
   switch (action) {
     case "create":
       console.log(`linking ${pkgNodeModulesPath}`)
       shell.ln('-sf', rootNodeModulesPath, pkgNodeModulesPath);
+      shell.ln('-sf', rootLintPath, pkgLintPath);
       break;
     case "delete":
       console.log(`removing ${pkgNodeModulesPath}`)
       shell.rm('-rf', pkgNodeModulesPath);
+      shell.rm('-rf', pkgLintPath);
       break;
     default:
       console.log(`action not recognized: ${action}`)
