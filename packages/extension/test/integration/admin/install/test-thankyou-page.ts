861fa15dc8d38cb5d2d8b5e7fff6a5c3df55b87b
Andrei Sabau <andrei@asabau.com>
2020-08-23 15:50:15 +0300

    removed no longer used extension popup, no longer relevant integration tests

--- packages/extension/test/integration/admin/install/test-thankyou-page.ts
+++ packages/extension/test/integration/admin/install/test-thankyou-page.ts
@@ -1,7 +1,7 @@
 import { assert } from 'chai';
 import { BrowserHelper, getBrowserHelper } from 'get-set-fetch-extension-test-utils';
 
-describe('Test ThankYou Pages', () => {
+describe('Install', () => {
   let browserHelper: BrowserHelper;
 
   before(async () => {
@@ -13,7 +13,7 @@ describe('Test ThankYou Pages', () => {
     await browserHelper.close();
   });
 
-  it('install page', async () => {
+  it('ThankYou Page', async () => {
     const tyPage = (await browserHelper.browser.pages()).find(page => /thank/.test(page.url()));
     assert.isDefined(tyPage);
     assert.equal(tyPage.url(), 'https://getsetfetch.org/thank-you-install.html');
--- packages/popup/README.md
+++
@@ -1,7 +0,0 @@
-# popup
-
-Responsible for handling toolbar integration.
-Opens the admin front-end into a new tab.
-
-## roadmap
-- add a dialog with the most commonly used features
