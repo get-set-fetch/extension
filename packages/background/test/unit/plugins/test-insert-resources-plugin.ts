1915473f25e5ee0c765ed7e5e44f9bdedbf520b3
Andrei Sabau <andrei@asabau.com>
2020-09-05 21:45:48 +0300

    unit test updates #51

--- packages/background/test/unit/plugins/test-insert-resources-plugin.ts
+++ packages/background/test/unit/plugins/test-insert-resources-plugin.ts
@@ -28,9 +28,9 @@ describe('Test Insert Resources Plugin', () => {
     sandbox.restore();
   });
 
-  it('when creating bloom filter for a site, also add its url as the 1st resource ', async () => {
+  it('when creating bloom filter for a site, also add its url as the 1st resource', async () => {
     insertResourcesPlugin = new InsertResourcesPlugin();
-    await insertResourcesPlugin.apply(site, {});
+    await insertResourcesPlugin.apply(site, { url: 'index.html' });
 
     // re-create the bloom filter based on its bitset
     assert.isDefined(site.resourceFilter);
@@ -40,42 +40,72 @@ describe('Test Insert Resources Plugin', () => {
     assert.isFalse(bloomFilter.test('other.html'));
   });
 
+  it('add redirect urls', async () => {
+    /*
+    when a resource is first saved its url is added to the filter
+    if the url is not present in filter afterwards, it means the resource url has changed due to redirect
+    the new url is added to the filter as well 
+    */
+    insertResourcesPlugin = new InsertResourcesPlugin();
+    await insertResourcesPlugin.apply(site, { url: 'redirect.html' });
+
+    // re-create the bloom filter based on its bitset
+    assert.isDefined(site.resourceFilter);
+    const bloomFilter = BloomFilter.create(insertResourcesPlugin.opts.maxEntries, insertResourcesPlugin.opts.probability, site.resourceFilter);
+
+    assert.isTrue(bloomFilter.test('redirect.html'));
+    assert.isFalse(bloomFilter.test('other.html'));
+  });
+
   it('multiple apply calls', async () => {
     insertResourcesPlugin = new InsertResourcesPlugin();
-    await insertResourcesPlugin.apply(site, { resourcesToAdd: [
+    await insertResourcesPlugin.apply(
+      site, 
+      { 
+        resourcesToAdd: [
+          {
+            url: 'pageA.html',
+            parent: {
+              linkText: 'pageA',
+            },
+          },
+          {
+            url: 'pageB.html',
+            parent: {
+              linkText: 'pageB',
+            },
+          },
+        ],
+        url: "index.html"
+      }
+    );
+
+    await insertResourcesPlugin.apply(
+      site, 
       {
-        url: 'pageA.html',
-        parent: {
-          linkText: 'pageA',
-        },
-      },
-      {
-        url: 'pageB.html',
-        parent: {
-          linkText: 'pageB',
-        },
-      },
-    ] });
-    await insertResourcesPlugin.apply(site, { resourcesToAdd: [
-      {
-        url: 'pageA.html',
-        parent: {
-          linkText: 'pageA',
-        },
-      },
-      {
-        url: 'pageB.html',
-        parent: {
-          linkText: 'pageB',
-        },
-      },
-      {
-        url: 'pageC.html',
-        parent: {
-          linkText: 'pageC',
-        },
-      },
-    ] });
+        resourcesToAdd: [
+          {
+            url: 'pageA.html',
+            parent: {
+              linkText: 'pageA',
+            },
+          },
+          {
+            url: 'pageB.html',
+            parent: {
+              linkText: 'pageB',
+            },
+          },
+          {
+            url: 'pageC.html',
+            parent: {
+              linkText: 'pageC',
+            },
+          },
+        ],
+        url: "index.html"
+      }
+    );
 
     // re-create the bloom filter based on its bitset
     assert.isDefined(site.resourceFilter);
